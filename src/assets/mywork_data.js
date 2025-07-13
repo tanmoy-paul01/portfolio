043d21504e72d63d3562f068765c5d7b4dadcc29
tnmoy-paul01 <tnmypl2003@gmail.com>
2025-07-13 15:51:09 +0530

    React dist file uploaded

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,31 +1,44 @@
 import project1_img from '../assets/project_1.png'
 import project2_img from '../assets/project_2.png'
-// import project3_img from '../assets/project_3.svg'
-// import project4_img from '../assets/project_4.svg'
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.png'
 // import project5_img from '../assets/project_5.svg'
 // import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Calculator",
-        w_img: project1_img,
-        w_Git_link: "https://github.com/tanmoy-paul01/Calculator",
-        w_Live_link: "https://tanmoy-paul01.github.io/Calculator/"
+        {
+        w_no: 4,
+        w_name: "QR Code Generator",
+        w_title: "This QR Code Generator is a full-stack web application built with HTML, CSS, JavaScript, Node.js, and Express.js. It allows users to input text or URLs and instantly generate downloadable QR codes. The app features a clean interface and fast performance, making it ideal for quick and secure data sharing.",
+        w_img: project4_img,
+        w_Git_link: "https://github.com/tanmoy-paul01/Generate_QR_Code",
+        w_Live_link: "https://generate-qr-code-2e8m.onrender.com/"
     },
     {
-        w_no:2,
-        w_name:"Tic-Tac-Toe",
+        w_no: 3,
+        w_name: "Free Lancing web application",
+        w_title: "A full-stack freelancing platform connecting skilled professionals with clients. Features include user profiles, job listings, and secure payment processing, with having learning opportunities for freelancers to enhance their skills, mentorship programs, and community forums for knowledge sharing.",
+        w_img: project3_img,
+        w_Git_link: "https://github.com/tanmoy-paul01/SkillMela",
+        w_Live_link: "https://skillmela.onrender.com/"
+    },
+    {
+        w_no: 2,
+        w_name: "Tic-Tac-Toe",
+        w_title: "Tic-Tac-Two is a classic two-player web game built with HTML, CSS, Javascript. Players take turns marking X or O on a 3x3 grid, aiming to align three symbols in a row. The game features a clean interface, responsive design, and smooth interactivity for an engaging and nostalgic gaming experience.",
         w_img: project2_img,
         w_Git_link: "https://github.com/tanmoy-paul01/Tic-Tac-Toe-Web-app",
         w_Live_link: "https://tanmoy-paul01.github.io/Tic-Tac-Toe-Web-app/"
     },
-    // {
-    //     w_no:3,
-    //     w_name:"Web design",
-    //     w_img: project3_img,
-    //     // w_link:
-    // },
+    {
+        w_no: 1,
+        w_name: "Calculator",
+        w_title: "This Calculator project is a responsive web application built using HTML, CSS, and JavaScript. It performs basic arithmetic operations like addition, subtraction, multiplication, and division. With a clean user interface and smooth button interactions, it’s designed for quick calculations on both desktop and mobile devices. Simple, efficient, and user-friendly.",
+        w_img: project1_img,
+        w_Git_link: "https://github.com/tanmoy-paul01/Calculator",
+        w_Live_link: "https://tanmoy-paul01.github.io/Calculator/"
+    },
+
     // {
     //     w_no:4,
     //     w_name:"Web design",
--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -11,14 +11,25 @@ const project = () => {
             <div className="project-container">
                 {mywork_data.map((work, index) => {
                     return (
+                        // <div key={index} className="project-card">
+                        //     <a href={work.w_Live_link} target='_blank'><img src={work.w_img} alt='' /></a>
+                        //     <div className="project-card-links">
+                        //         <a href={work.w_Live_link} target='_blank'>Live vew</a>
+                        //         <a href={work.w_Git_link} target='_blank'>Github Repo</a>
+                        //     </div>
+                        // </div>
                         <div key={index} className="project-card">
-                            <a href={work.w_Live_link} target='_blank'><img src={work.w_img} alt='' /></a>
-                            <div className="project-card-links">
-                                <a href={work.w_Live_link} target='_blank'>Live vew</a>
-                                <a href={work.w_Git_link} target='_blank'>Github Repo</a>
+                            <div className="project-image-container">
+                                <img src={work.w_img} alt='' />
+                                <div className="project-overlay">
+                                    <p>{work.w_title}</p>
+                                    <div className="project-card-links">
+                                        <a href={work.w_Live_link} target='_blank' rel="noreferrer">Live View</a>
+                                        <a href={work.w_Git_link} target='_blank' rel="noreferrer">GitHub Repo</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
                     )
                 })}
             </div>
