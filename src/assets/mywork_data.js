c5cacd6e602370b6759b93c55957bf235fb27113
tnmoy-paul01 <tnmypl2003@gmail.com>
2025-08-06 21:50:29 +0530

    project5-add

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,11 +2,19 @@ import project1_img from '../assets/project_1.png'
 import project2_img from '../assets/project_2.png'
 import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
-// import project5_img from '../assets/project_5.svg'
+import project5_img from '../assets/project_5.png'
 // import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
-        {
+    {
+        w_no: 5,
+        w_name: "Doctor Appointment System",
+        w_title: "DocDirect is a doctor appointment web app built with the MERN stack, styled using Tailwind CSS. It features secure JWT authentication, encrypted passwords with bcrypt, image uploads via Cloudinary, and email services using Nodemailer. The platform streamlines doctor-patient interactions through easy appointment booking and reliable communication.",
+        w_img: project5_img,
+        w_Git_link: "https://github.com/tanmoy-paul01/DocDirect",
+        w_Live_link: "https://docdirect-frontend.onrender.com/"
+    },
+    {
         w_no: 4,
         w_name: "QR Code Generator",
         w_title: "This QR Code Generator is a full-stack web application built with HTML, CSS, JavaScript, Node.js, and Express.js. It allows users to input text or URLs and instantly generate downloadable QR codes. The app features a clean interface and fast performance, making it ideal for quick and secure data sharing.",
